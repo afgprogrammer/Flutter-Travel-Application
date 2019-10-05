c5eb98a0ebcdd6375311d20b59a83c02038825ec
Mohammad Rahmani <rto1680@gmail.com>
2019-10-05 20:43:33 +0430

    Make main page design

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:day11/Animation/FadeAnimation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -13,12 +14,134 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.grey[100],
+      backgroundColor: Colors.grey[100],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.jpg'),
+                  fit: BoxFit.cover
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.2),
+                    ]
+                  )
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    FadeAnimation(1, Text("What you would like to find?", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
+                    SizedBox(height: 30,),
+                    FadeAnimation(1.3, Container(
+                      padding: EdgeInsets.symmetric(vertical: 3),
+                      margin: EdgeInsets.symmetric(horizontal: 40),
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, color: Colors.grey,),
+                          hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                          hintText: "Search for cities, places ..."
+                        ),
+                      ),
+                    )),
+                    SizedBox(height: 30,)
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(1, Text("Best Destination", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 20),)),
+                  SizedBox(height: 20,),
+                  FadeAnimation(1.4, Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        makeItem(image: 'assets/images/canada.jpg', title: 'Canada'),
+                        makeItem(image: 'assets/images/Italy.jpg', title: 'Italy'),
+                        makeItem(image: 'assets/images/greece.jpg', title: 'Greece'),
+                        makeItem(image: 'assets/images/united-states.jpg', title: 'United States')
+                      ],
+                    ),
+                  )),
+
+                  SizedBox(height: 20,),
+                  FadeAnimation(1, Text("Best Hotels", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 20),)),
+                  SizedBox(height: 20,),                
+                  FadeAnimation(1.4, Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        makeItem(image: 'assets/images/united-states.jpg', title: 'Canada'),
+                        makeItem(image: 'assets/images/greece.jpg', title: 'Italy'),
+                        makeItem(image: 'assets/images/Italy.jpg', title: 'Greece'),
+                        makeItem(image: 'assets/images/canada.jpg', title: 'United States')
+                      ],
+                    ),
+                  )),
+                  SizedBox(height: 80,),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget makeItem({image, title}) {
+    return AspectRatio(
+      aspectRatio: 1 / 1,
+      child: Container(
+        margin: EdgeInsets.only(right: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            image: AssetImage(image),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.2),
+              ]
+            )
+          ),
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(title, style: TextStyle(color: Colors.white, fontSize: 20),),
+          ),
+        ),
       ),
     );
   }
